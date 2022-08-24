b1b4b4f99b33074d73e0074e562a217bd408c406
İsmail Baran KARASU <ismailbaran04@gmail.com>
2022-08-24 21:27:33 +0300

    duzenle sayfası tasarımı değiştirildi

--- duzenle.php
+++ duzenle.php
@@ -1,58 +1,42 @@
-<?php require_once 'baglan.php'; ?>
-<!DOCTYPE html>
-<html>
-<head>
-	<title>CRUD İŞLEMLERİ</title>
-</head>
+<?php include 'header.php'; ?>
 <body>
-
-
-	<h1>Veritabanı PDO düzenleme işlemleri</h1>
-	<hr>
-	<?php /* 
-	if ($_GET['durum']=="ok") {
-		
-		echo "İşlem başarılı";
-
-	} elseif ($_GET['durum']=="no") {
-
-		echo "İşlem başarısız";
-
-
-	}
-
-	*/?>
-
 	<?php 
 
 	$bilgilerimsor=$db->prepare("SELECT * from bilgilerim where bilgilerim_id=:id");
-	$bilgilerimsor->execute(array(
+	$bilgilerimsor->execute([
 		'id' => $_GET['bilgilerim_id']
-	));
+	]);
 
 	$bilgilerimcek=$bilgilerimsor->fetch(PDO::FETCH_ASSOC);
 
 	?>
-
-	<form action="islem.php" method="POST">
-
-		<input type="text" required="" name="bilgilerim_ad" value="<?php echo $bilgilerimcek['bilgilerim_ad'] ?>">
-		<input type="text" required="" name="bilgilerim_soyad" value="<?php echo $bilgilerimcek['bilgilerim_soyad'] ?>">
-		<input type="email" required="" name="bilgilerim_mail" value="<?php echo $bilgilerimcek['bilgilerim_mail'] ?>">
-		<input type="text" required="" name="bilgilerim_yas" value="<?php echo $bilgilerimcek['bilgilerim_yas'] ?>">
-
-		<input type="hidden" value="<?php echo $bilgilerimcek['bilgilerim_id'] ?>" name="bilgilerim_id">
-		<button type="submit" name="updateislemi">Formu Düzenle</button>
-
-	</form>
-
-	<br>
-
-
-
-
-
-
-
+    <section>
+        <div class="container w-25 mx-auto">
+            <form action="islem.php" method="POST">
+                <div class="w-100 mb-3 bg-danger text-white rounded-2">
+                    <span>Kayıt Düzenle</span>
+                </div>
+                <div class="w-100 mb-3">
+                    <input type="text" name="bilgilerim_ad" class="form-control border border-info" value="<?php echo $bilgilerimcek['bilgilerim_ad'] ?>" required="">
+                </div>
+                <div class="w-100 mb-3">
+                <input type="text" name="bilgilerim_soyad" class="form-control border border-info" value="<?php echo $bilgilerimcek['bilgilerim_soyad'] ?>" required="">
+                </div>
+                <div class="w-100 mb-3">
+                <input type="email" name="bilgilerim_mail" class="form-control border border-info" value="<?php echo $bilgilerimcek['bilgilerim_mail'] ?>" required="">
+                </div>
+                <div class="w-100 mb-3">
+                <input type="text" name="bilgilerim_yas" class="form-control border border-info" value="<?php echo $bilgilerimcek['bilgilerim_yas'] ?>" required="">
+                </div>
+                <div class="w-100 mb-3">
+                <input type="hidden" value="<?php echo $bilgilerimcek['bilgilerim_id'] ?>" name="bilgilerim_id">
+                </div>
+                </div>
+                <div class="w-25 girisyap">
+                    <button type="submit" name="updateislemi" class="btn btn-outline-danger">Düzenle</button>
+                </div>
+            </form>
+        </div>
+    </section>
 </body>
 </html>
